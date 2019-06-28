b3f30b478e854011768cd3d4abcedd9a243f6e94
Albert Dugba <albert.dugba@gmail.com>
2019-06-28 22:09:08 +0000

    Updates

---
+++ precache-manifest.dba81f0969e7810d74d62241cacd64a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e96a3a186148a7c8b4cfeb0463e841",
+    "url": "/firebase-note-react-app/index.html"
+  },
+  {
+    "revision": "24834c8e0232e828947f",
+    "url": "/firebase-note-react-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8653e5c7b361c0b43cb4",
+    "url": "/firebase-note-react-app/static/css/main.f69937c9.chunk.css"
+  },
+  {
+    "revision": "24834c8e0232e828947f",
+    "url": "/firebase-note-react-app/static/js/2.5bc654be.chunk.js"
+  },
+  {
+    "revision": "8653e5c7b361c0b43cb4",
+    "url": "/firebase-note-react-app/static/js/main.29787d9c.chunk.js"
+  },
+  {
+    "revision": "eff2022cdee894db1036",
+    "url": "/firebase-note-react-app/static/js/runtime~main.630e5dd8.js"
+  }
+]);
